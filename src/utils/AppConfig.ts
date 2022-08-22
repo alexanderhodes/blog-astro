f8f085044ebeda2b287088b3dc0a7dd98ab69e3a
Alexander Hodes <alexander.hodes@live.com>
2022-08-22 17:49:40 +0200

    feat: changed app config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
+  site_name: 'Blog von Alexander',
   title: 'Astro boilerplate',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
-  locale: 'en',
+  author: 'Alexander',
+  locale_region: 'de-de',
+  locale: 'de',
 };
